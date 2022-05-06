0123ccb0b6f7c5078a7da737d0c2eacfadcf6dca
cidrblock <bthornto@redhat.com>
2022-05-06 13:53:50 -0700

    Error after

--- src/pytest_ansible_network_integration/__init__.py
+++ src/pytest_ansible_network_integration/__init__.py
@@ -356,6 +356,7 @@ def github_log(request: pytest.FixtureRequest) -> Generator[None, None, None]:
 
         if hasattr(request.node, "rep_call"):
             if request.node.rep_setup.passed and request.node.rep_call.failed:
+                _github_action_log("::endgroup::")
                 _github_action_log(f"::error title=Integration test failure::{name}")
 
         _github_action_log("::endgroup::")
