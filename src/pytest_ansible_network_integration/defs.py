e2432bcba429e5df7c019a7d1b46d3230c3855ec
cidrblock <bthornto@redhat.com>
2022-05-12 12:53:03 -0700

    Typo

--- src/pytest_ansible_network_integration/defs.py
+++ src/pytest_ansible_network_integration/defs.py
@@ -148,7 +148,7 @@ class CmlWrapper:
             return
         # In the case of GH actions store the labs in an env var for clean up if the job is
         # cancelled, this is referenced in the GH integration workflow
-        if os.environment.get("CML_LABS"):
+        if os.environ.get("CML_LABS"):
             os.environ["CML_LABS"] += f",{self.current_lab_id}"
         else:
             os.environ["CML_LABS"] = self.current_lab_id
